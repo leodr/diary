8423ef8b72a7e1e22d194da9d440df8400d92916
Leo Driesch <dev@leodriesch.com>
2022-05-07 16:09:21 +0200

    Change selection colors

--- src/components/editor-parts/ContentEditable.tsx
+++ src/components/editor-parts/ContentEditable.tsx
@@ -1,6 +1,7 @@
 import LexicalContentEditable from "@lexical/react/LexicalContentEditable";
 import { motion, useMotionValue } from "framer-motion";
 import { useLayoutEffect } from "react";
+import { classNames } from "../../utils/classnames";
 
 export function ContentEditable() {
   const minHeight = useMotionValue(520);
@@ -19,7 +20,11 @@ export function ContentEditable() {
 
   return (
     <MotionLexicalContentEditable
-      className="outline-none text-gray-700 dark:text-gray-200"
+      className={classNames(
+        "outline-none",
+        "text-gray-700 selection:text-gray-50 selection:bg-gray-900",
+        "dark:text-gray-200 dark:selection:bg-gray-100 dark:selection:text-gray-900"
+      )}
       style={{ minHeight }}
     />
   );
